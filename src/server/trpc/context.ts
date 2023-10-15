1dc0d3ccdcdbec540d65955d9cb46a8c432d1ced
Todd Dukart <tdukart@mechanicalninja.com>
2023-10-15 19:49:53 -0400

    Bugfix in import

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -2,7 +2,7 @@ import { inferAsyncReturnType } from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { getServerSession } from 'next-auth';
 
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { authOptions } from '@/app/auth/[...nextauth]/route';
 
 /**
  * Creates context for an incoming request
